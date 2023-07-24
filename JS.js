4355d3331719820041df423ebebb09e9b00270ec
SatwikSPrabhu <117463364+SatwikSPrabhu@users.noreply.github.com>
2023-07-24 22:57:30 +0530

    Add files via upload

--- JS.js
+++ JS.js
@@ -13,35 +13,74 @@ const countdownForm = document.getElementById('countdown-form');
 const secondsInput = document.getElementById('seconds');
 let count = 0;
 let running = false;
+let paused = false;
 let timer = null;
+let userInputValue = 0;
 
 function update() {
   displayedTime.textContent = clock.format(count);
 }
 
 function handleStart() {
-  timer = setInterval(() => {
-    const newCount = count - 1;
-    count = newCount >= 0 ? newCount : 0;
-    update();
-  }, 1000);
-}
-
-function handleStop() {
-  if (timer) {
-    clearInterval(timer);
-    running = false;
+  if (!running) {
+    running = true;
+    timer = setInterval(() => {
+      if (!paused) {
+        const newCount = count - 1;
+        count = newCount >= 0 ? newCount : 0;
+        update();
+      }
+    }, 1000);
   }
 }
 
+function handlePause() {
+  if (timer) {
+    clearInterval(timer);
+    running = false;
+    paused = true;
+    pauseBtn.textContent = 'Resume';
+  }
+}
+
+function handleResume() {
+  if (!running) {
+    running = true;
+    paused = false;
+
+    pauseBtn.textContent = 'Pause';
+
+    timer = setInterval(() => {
+      if (!paused) {
+        const newCount = count - 1;
+        count = newCount >= 0 ? newCount : 0;
+        update();
+      }
+    }, 1000);
+  }
+}
+
+function handleRestart() {
+  handlePause();
+  count = userInputValue;
+  paused = false;
+  update();
+
+  pauseBtn.textContent = 'Pause';
+}
+
 function handleReset() {
+  handlePause();
   count = 0;
   update();
+
+  pauseBtn.textContent = 'Pause';
 }
 
 function handleCountdown(seconds) {
+  userInputValue = seconds;
   count = seconds;
-  running = true;
+  paused = false;
   handleStart();
 }
 
@@ -54,14 +93,23 @@ countdownForm.addEventListener('submit', (event) => {
   }
 });
 
-const stopBtn = document.getElementById('stopBtn');
-stopBtn.addEventListener('click', () => {
-  handleStop();
+const pauseBtn = document.getElementById('pauseBtn');
+pauseBtn.addEventListener('click', () => {
+  if (paused) {
+    handleResume();
+  } else {
+    handlePause();
+  }
 });
-
 const resetBtn = document.getElementById('resetBtn');
 resetBtn.addEventListener('click', () => {
   handleReset();
+});
+
+const restartBtn = document.getElementById('restart');
+restartBtn.addEventListener('click', () => {
+  handleRestart();
+  handleStart();
 });
 
 update();
